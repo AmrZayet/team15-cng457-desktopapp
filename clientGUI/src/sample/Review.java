1265f1866e0f5553d3c58bb26734633aab6702fd
mertksk <mert_ksk_1998@hotmail.com>
2021-06-19 14:21:58 +0300

    Solved Conflict with Master

--- clientGUI/src/sample/Review.java
+++ clientGUI/src/sample/Review.java
@@ -1,9 +1,23 @@
 package sample;
 
 public class Review {
-    
+
     private int revID;
     private String comment;
     private int rate;
 
+    public Review(int revID, String comment, int rate) {
+        this.revID = revID;
+        this.comment = comment;
+        this.rate = rate;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "revID=" + revID +
+                ", comment='" + comment + '\'' +
+                ", rate=" + rate +
+                '}';
+    }
 }
