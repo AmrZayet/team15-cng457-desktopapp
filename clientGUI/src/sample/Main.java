eea1b6f9b9c288dbb85a5c6991625066e2a3f063
mertksk <mert_ksk_1998@hotmail.com>
2021-06-20 10:59:15 +0300

    Chanced the Tittle

--- clientGUI/src/sample/Main.java
+++ clientGUI/src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Online Market");
         primaryStage.setScene(new Scene(root, 850, 750));
         primaryStage.show();
     }
