797e297b0c4e43cd7cd1d226e594f1a22b2db534
Amr Zayet <amrz.edu@gmail.com>
2021-06-27 16:23:18 +0300

    Deleted the demo folder. Note: The webservice updates are in the WebService repon now.

--- demo/src/main/java/com/example/demo/Repository/PhoneFeatureRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.entity.PhoneFeature;
-import com.example.demo.entity.PhoneFeatureID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhoneFeatureRepository extends JpaRepository<PhoneFeature, PhoneFeatureID> {
-}
